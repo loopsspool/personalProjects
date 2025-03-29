1c5b4b3b369516ff9ac1488b1a069e47161ee658
loopsspool <45133123+loopsspool@users.noreply.github.com>
2025-03-29 10:44:18 -0500

    Started form db population

--- Pokeball-Pokemon-Comparison/Scripts/db_utils.py
+++ Pokeball-Pokemon-Comparison/Scripts/db_utils.py
@@ -64,7 +64,6 @@ def db_exists():
     return os.path.exists(DB_PATH)
 
 def insert_poke(cursor, num, num_as_text, name, gen):
-    # Doesn't need num bc of autoincrement, and parsing thru info_spreadsheet in ascending order
     cursor.execute("""
         INSERT OR IGNORE INTO pokemon (num, num_as_text, name, gen)
         VALUES (?, ?, ?, ?);
@@ -73,12 +72,38 @@ def insert_poke(cursor, num, num_as_text, name, gen):
 def populate_pokes(cursor):
     print("Populating Pokemon into database...")
 
+    # Grabbing information from pokemon info spreadsheet
     for row in range(2, pokemon_info_sheet.max_row + 1):
         num = row - 1
         num_as_str = str(cell_value(pokemon_info_sheet, row, poke_info_num_col))
         name = cell_value(pokemon_info_sheet, row, poke_info_name_col)
         gen = cell_value(pokemon_info_sheet, row, poke_info_gen_col)
         insert_poke(cursor, num, num_as_str, name, gen)
+
+def insert_forms(cursor, poke_num, form_name):
+    cursor.execute("""
+        INSERT OR IGNORE INTO forms (poke_num, form_name)
+        VALUES (?, ?);
+    """, (poke_num, form_name))
+
+def populate_forms(cursor):
+    print("Populating forms into database...")
+
+    forms = []
+    # TODO: Here is where you'll have to import all the hardcoded exceptions and pokemon-by-pokemon individualities
+    for row in range(2, pokemon_info_sheet.max_row + 1):
+        forms.clear()
+        poke_num = int(cell_value(pokemon_info_sheet, row, poke_info_num_col))
+        female_field = cell_value(pokemon_info_sheet, row, poke_info_f_col)
+        mega_field = cell_value(pokemon_info_sheet, row, poke_info_mega_col)
+        giganta_field = cell_value(pokemon_info_sheet, row, poke_info_giganta_col)
+        regional_form_field = cell_value(pokemon_info_sheet, row, poke_info_reg_forms_col)
+        misc_form_field = cell_value(pokemon_info_sheet, row, poke_info_misc_forms_col)
+        #misc_forms = [item.strip() for item in misc_form_field.split(",")]
+
+        if is_x_or_num(female_field): forms.append("f")
+        if is_x_or_num(mega_field): forms.append("Mega")
+        if is_x_or_num(giganta_field): forms.append("Gigantamax")
 
 
 def populate_db():
@@ -90,6 +115,7 @@ def populate_db():
 
     try:
         populate_pokes(cursor)
+        populate_forms(cursor)
 
         connection.commit()
     except Exception as e:
