f4ad87cff8a2154a83fdc04fd91615ccd4eea493
loopsspool <45133123+loopsspool@users.noreply.github.com>
2025-03-29 22:03:13 -0500

    Added TODOs

--- Pokeball-Pokemon-Comparison/Scripts/filename_changer.py
+++ Pokeball-Pokemon-Comparison/Scripts/filename_changer.py
@@ -1,6 +1,8 @@
 import os
 import re
 
+# TODO: No form tags in drawn images
+
 game_sprite_path = "C:\\Users\\ethan\\OneDrive\\Desktop\\Code\\Pokeball-Pokemon-Comparison\\Images\\Pokemon\\Game Sprites\\"
 back_sprites_to_filter_path = "C:\\Users\\ethan\\OneDrive\\Desktop\\Code\\Pokeball-Pokemon-Comparison\\Images\\Pokemon\\Game Sprites\\back_imgs_to_be_filtered"
 animated_pngs_pre_gif_conversion = "C:\\Users\\ethan\\OneDrive\\Desktop\\Code\\Pokeball-Pokemon-Comparison\\Images\\Pokemon\\Game Sprites\\animated_pngs_for_gifs\\pngs\\Converted to gif"
@@ -41,7 +43,6 @@ def replace_in_filename(path, replace, replace_with):
                 new_full_path = os.path.join(path, new_f)
                 print(new_f)
                 os.rename(full_path, new_full_path)
-            # TODO: have BDSP & SwSh share images, but not LA
 
 # Shiny tag first
 # Then form (AND add -Form-____ tag to misc/type forms)
