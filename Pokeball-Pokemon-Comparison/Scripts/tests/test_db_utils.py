5c3b22b6d85d658b7a487f7665d0f69b8af5697f
loopsspool <45133123+loopsspool@users.noreply.github.com>
2025-04-05 09:14:15 -0500

    initializing framework for testing

--- Pokeball-Pokemon-Comparison/Scripts/tests/test_db_utils.py
+++ Pokeball-Pokemon-Comparison/Scripts/tests/test_db_utils.py
@@ -5,7 +5,9 @@ import os
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 
-from db_utils import FORM_EXCLUSIONS, is_form_available
+from db_utils import FORM_EXCLUSIONS, is_form_available, get_poke_form_records, get_game_records
 
-
+def test_is_form_available():
+    # TODO: Will have to create connection to db
+    is_form_available()
 
