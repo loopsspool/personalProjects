28f3e9d2b41a2f3b4178daeede9b868988ddd379
loopsspool <45133123+loopsspool@users.noreply.github.com>
2025-04-06 20:27:00 -0500

    Added more exclusionary integration tests

--- Pokeball-Pokemon-Comparison/Scripts/tests/test_db_utils.py
+++ Pokeball-Pokemon-Comparison/Scripts/tests/test_db_utils.py
@@ -14,9 +14,29 @@ def test_is_form_obtainable():
     poke_forms = get_poke_form_records(cursor)
     games = get_game_records(cursor)
     connection.close()
-    # TODO: Insert specific pokemon here that fulfill each check in FORM_EXCLUSIONS
+    # Species game availability
     assert FORM_EXCLUSIONS["filtering_for_LGPE_dex_if_needed"](poke_forms[(152, 1)], games[16]) == True
     assert FORM_EXCLUSIONS["filtering_for_LGPE_dex_if_needed"](poke_forms[(1, 1)], games[16]) == False
+    assert FORM_EXCLUSIONS["filtering_for_SwSh_dex_if_needed"](poke_forms[(13, 1)], games[17]) == True
+    assert FORM_EXCLUSIONS["filtering_for_SwSh_dex_if_needed"](poke_forms[(1, 1)], games[17]) == False
+    assert FORM_EXCLUSIONS["filtering_for_BDSP_dex_if_needed"](poke_forms[(494, 1)], games[18]) == True
+    assert FORM_EXCLUSIONS["filtering_for_BDSP_dex_if_needed"](poke_forms[(1, 1)], games[18]) == False
+    assert FORM_EXCLUSIONS["filtering_for_LA_dex_if_needed"](poke_forms[(1, 1)], games[19]) == True
+    assert FORM_EXCLUSIONS["filtering_for_LA_dex_if_needed"](poke_forms[(25, 1)], games[19]) == False
+    assert FORM_EXCLUSIONS["filtering_for_SV_dex_if_needed"](poke_forms[(10, 1)], games[20]) == True
+    assert FORM_EXCLUSIONS["filtering_for_SV_dex_if_needed"](poke_forms[(1, 1)], games[20]) == False
 
-    
+    # Universal rules
+    assert FORM_EXCLUSIONS["no_pokemon_with_a_higher_generation_than_game_generation"](poke_forms[(152, 1)], games[1]) == True
+    assert FORM_EXCLUSIONS["no_pokemon_with_a_higher_generation_than_game_generation"](poke_forms[(1025, 1)], games[20]) == False
+    assert FORM_EXCLUSIONS["no_f_form_visual_differences_before_gen_4"](poke_forms[(3, 4)], games[9]) == True
+    assert FORM_EXCLUSIONS["no_f_form_visual_differences_before_gen_4"](poke_forms[(3, 4)], games[10]) == False
+    assert FORM_EXCLUSIONS["no_fairy_forms_before_gen_6"](poke_forms[(493, 759)], games[13]) == True
+    assert FORM_EXCLUSIONS["no_fairy_forms_before_gen_6"](poke_forms[(493, 759)], games[14]) == False
+    assert FORM_EXCLUSIONS["no_megas_outside_gen_6_and_7_excluding_LGPE"](poke_forms[(6, 11)], games[13]) == True
+    assert FORM_EXCLUSIONS["no_megas_outside_gen_6_and_7_excluding_LGPE"](poke_forms[(6, 11)], games[14]) == False
+    assert FORM_EXCLUSIONS["no_megas_outside_gen_6_and_7_excluding_LGPE"](poke_forms[(6, 11)], games[17]) == True
+    assert FORM_EXCLUSIONS["no_megas_outside_gen_6_and_7_excluding_LGPE"](poke_forms[(3, 5)], games[13]) == True
+    assert FORM_EXCLUSIONS["no_megas_outside_gen_6_and_7_excluding_LGPE"](poke_forms[(3, 5)], games[14]) == False
+    assert FORM_EXCLUSIONS["no_megas_outside_gen_6_and_7_excluding_LGPE"](poke_forms[(3, 5)], games[17]) == True
 
