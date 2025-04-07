fd993aa9058890a9a933573452590229c98024ce
loopsspool <45133123+loopsspool@users.noreply.github.com>
2025-04-07 09:43:50 -0500

    Refactored to have pytest parametrize test lambda calls

--- Pokeball-Pokemon-Comparison/Scripts/tests/test_db_utils.py
+++ Pokeball-Pokemon-Comparison/Scripts/tests/test_db_utils.py
@@ -1,42 +1,52 @@
 import sys
 import os
 import sqlite3
+import pytest
 
 # To go into parent directory to import db_utils
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 
-from db_utils import DB_PATH, FORM_EXCLUSIONS, is_form_obtainable, get_poke_form_records, get_game_records
+from db_utils import DB_PATH, FORM_EXCLUSIONS, is_form_obtainable, get_poke_form_records, get_game_records, get_form_id, get_game_id
 
-def test_is_form_obtainable():
-    connection = sqlite3.connect(DB_PATH)
-    cursor = connection.cursor()
-    poke_forms = get_poke_form_records(cursor)
-    games = get_game_records(cursor)
-    connection.close()
+# Getting necessary info for testing
+connection = sqlite3.connect(DB_PATH)
+cursor = connection.cursor()
+default_form_id = get_form_id("Default")
+poke_forms = get_poke_form_records(cursor)
+games = get_game_records(cursor)
+connection.close()
+
+@pytest.mark.parametrize("form_id, game_id, expected", [
     # Species game availability
-    assert FORM_EXCLUSIONS["filtering_for_LGPE_dex_if_needed"](poke_forms[(152, 1)], games[16]) == True
-    assert FORM_EXCLUSIONS["filtering_for_LGPE_dex_if_needed"](poke_forms[(1, 1)], games[16]) == False
-    assert FORM_EXCLUSIONS["filtering_for_SwSh_dex_if_needed"](poke_forms[(13, 1)], games[17]) == True
-    assert FORM_EXCLUSIONS["filtering_for_SwSh_dex_if_needed"](poke_forms[(1, 1)], games[17]) == False
-    assert FORM_EXCLUSIONS["filtering_for_BDSP_dex_if_needed"](poke_forms[(494, 1)], games[18]) == True
-    assert FORM_EXCLUSIONS["filtering_for_BDSP_dex_if_needed"](poke_forms[(1, 1)], games[18]) == False
-    assert FORM_EXCLUSIONS["filtering_for_LA_dex_if_needed"](poke_forms[(1, 1)], games[19]) == True
-    assert FORM_EXCLUSIONS["filtering_for_LA_dex_if_needed"](poke_forms[(25, 1)], games[19]) == False
-    assert FORM_EXCLUSIONS["filtering_for_SV_dex_if_needed"](poke_forms[(10, 1)], games[20]) == True
-    assert FORM_EXCLUSIONS["filtering_for_SV_dex_if_needed"](poke_forms[(1, 1)], games[20]) == False
+    ((152, default_form_id), get_game_id("LGPE"), False),
+    ((1, default_form_id), get_game_id("LGPE"), True),
+    ((13, default_form_id), get_game_id("SwSh"), False),
+    ((1, default_form_id), get_game_id("SwSh"), True),
+    ((494, default_form_id), get_game_id("BDSP"), False),
+    ((1, default_form_id), get_game_id("BDSP"), True),
+    ((1, default_form_id), get_game_id("LA"), False),
+    ((25, default_form_id), get_game_id("LA"), True),
+    ((10, default_form_id), get_game_id("SV"), False),
+    ((1, default_form_id), get_game_id("SV"), True),
 
     # Universal rules
-    assert FORM_EXCLUSIONS["no_pokemon_with_a_higher_generation_than_game_generation"](poke_forms[(152, 1)], games[1]) == True
-    assert FORM_EXCLUSIONS["no_pokemon_with_a_higher_generation_than_game_generation"](poke_forms[(1025, 1)], games[20]) == False
-    assert FORM_EXCLUSIONS["no_f_form_visual_differences_before_gen_4"](poke_forms[(3, 4)], games[9]) == True
-    assert FORM_EXCLUSIONS["no_f_form_visual_differences_before_gen_4"](poke_forms[(3, 4)], games[10]) == False
-    assert FORM_EXCLUSIONS["no_fairy_forms_before_gen_6"](poke_forms[(493, 759)], games[13]) == True
-    assert FORM_EXCLUSIONS["no_fairy_forms_before_gen_6"](poke_forms[(493, 759)], games[14]) == False
-    assert FORM_EXCLUSIONS["no_megas_outside_gen_6_and_7_excluding_LGPE"](poke_forms[(6, 11)], games[13]) == True
-    assert FORM_EXCLUSIONS["no_megas_outside_gen_6_and_7_excluding_LGPE"](poke_forms[(6, 11)], games[14]) == False
-    assert FORM_EXCLUSIONS["no_megas_outside_gen_6_and_7_excluding_LGPE"](poke_forms[(6, 11)], games[17]) == True
-    assert FORM_EXCLUSIONS["no_megas_outside_gen_6_and_7_excluding_LGPE"](poke_forms[(3, 5)], games[13]) == True
-    assert FORM_EXCLUSIONS["no_megas_outside_gen_6_and_7_excluding_LGPE"](poke_forms[(3, 5)], games[14]) == False
-    assert FORM_EXCLUSIONS["no_megas_outside_gen_6_and_7_excluding_LGPE"](poke_forms[(3, 5)], games[17]) == True
+    ((152, default_form_id), 1, False),
+    ((1025, default_form_id), 20, True),
+    ((3, get_form_id("-f")), get_game_id("FRLG"), False),
+    ((3, get_form_id("-f")), get_game_id("Diamond-Pearl"), True),
+    ((493, get_form_id("-Form-Fairy")), get_game_id("BW-B2W2"), False),
+    ((493, get_form_id("-Form-Fairy")), get_game_id("XY-ORAS"), True),
+    ((6, get_form_id("-Mega_X")), get_game_id("BW-B2W2"), False),
+    ((6, get_form_id("-Mega_X")), get_game_id("XY-ORAS"), True),
+    ((6, get_form_id("-Mega_X")), get_game_id("LGPE"), False),
+    ((6, get_form_id("-Mega_X")), get_game_id("SwSh"), False),
+    ((3, get_form_id("-Mega")), get_game_id("BW-B2W2"), False),
+    ((3, get_form_id("-Mega")), get_game_id("XY-ORAS"), True),
+    ((3, get_form_id("-Mega")), get_game_id("LGPE"), False),
+    ((3, get_form_id("-Mega")), get_game_id("SwSh"), False)
+])
 
+def test_is_form_obtainable(form_id, game_id, expected):
+    # Getting the dict values that holds poke_form_info and game_info, respectively, to check obtainability
+    assert is_form_obtainable(poke_forms[form_id], games[game_id]) == expected
