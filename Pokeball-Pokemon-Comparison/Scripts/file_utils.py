decdf0458b1c399e14c1203f82eb9f45d02d2d0c
loopsspool <45133123+loopsspool@users.noreply.github.com>
2025-04-13 15:11:29 -0500

    Added ability to just print proposed changes

--- Pokeball-Pokemon-Comparison/Scripts/file_utils.py
+++ Pokeball-Pokemon-Comparison/Scripts/file_utils.py
@@ -6,6 +6,7 @@ import re
 game_sprite_path = "C:\\Users\\ethan\\OneDrive\\Desktop\\Code\\Pokeball-Pokemon-Comparison\\Images\\Pokemon\\Game Sprites\\"
 back_sprites_to_filter_path = "C:\\Users\\ethan\\OneDrive\\Desktop\\Code\\Pokeball-Pokemon-Comparison\\Images\\Pokemon\\Game Sprites\\back_imgs_to_be_filtered"
 animated_pngs_pre_gif_conversion = "C:\\Users\\ethan\\OneDrive\\Desktop\\Code\\Pokeball-Pokemon-Comparison\\Images\\Pokemon\\Game Sprites\\animated_pngs_for_gifs\\pngs\\Converted to gif"
+test_path = "C:\\Users\\ethan\\OneDrive\\Desktop\\Code\\Pokeball-Pokemon-Comparison\\Images\\Pokemon\\Test"
 file_ext = ""
 
 def add_leading_zero(path):
@@ -17,7 +18,7 @@ def add_leading_zero(path):
             print(new_full_path)
             os.rename(full_path, new_full_path)
 
-def replace_in_filename(path, replace, replace_with):
+def replace_in_filename(path, replace, replace_with, just_print=False):
     files = os.listdir(path)
     for f in files:
         full_path = os.path.join(path, f)
@@ -25,16 +26,18 @@ def replace_in_filename(path, replace, replace_with):
             if replace in f:
                 new_f = f.replace(replace, replace_with)
                 new_full_path = os.path.join(path, new_f)
-                print(new_f)
-                os.rename(full_path, new_full_path)
+                print(f"{f}\t changed to \t{new_f}")
+                if not just_print:
+                    os.rename(full_path, new_full_path)
 
-def replace_filename_in_all_dirs(replace, replace_with):
-    replace_in_filename(game_sprite_path, replace, replace_with)
-    replace_in_filename(back_sprites_to_filter_path, replace, replace_with)
-    replace_in_filename(animated_pngs_pre_gif_conversion, replace, replace_with)
+def replace_filename_in_all_dirs(replace, replace_with, just_print=False):
+    replace_in_filename(game_sprite_path, replace, replace_with, just_print)
+    replace_in_filename(back_sprites_to_filter_path, replace, replace_with, just_print)
+    replace_in_filename(animated_pngs_pre_gif_conversion, replace, replace_with, just_print)
 
 def print_files_with(path, s):
     files = os.listdir(path)
     for f in files:
         if s in f:
             print(f)
+
