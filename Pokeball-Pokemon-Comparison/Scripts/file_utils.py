02b7d48ffcf9dc90164f02f2cc83d4675678445c
loopsspool <45133123+loopsspool@users.noreply.github.com>
2025-04-13 17:37:04 -0500

    Reformatted file

--- Pokeball-Pokemon-Comparison/Scripts/file_utils.py
+++ Pokeball-Pokemon-Comparison/Scripts/file_utils.py
@@ -1,13 +1,13 @@
 import os
-import re
 
-# TODO: No form tags in drawn images
 
 game_sprite_path = "C:\\Users\\ethan\\OneDrive\\Desktop\\Code\\Pokeball-Pokemon-Comparison\\Images\\Pokemon\\Game Sprites\\"
 back_sprites_to_filter_path = "C:\\Users\\ethan\\OneDrive\\Desktop\\Code\\Pokeball-Pokemon-Comparison\\Images\\Pokemon\\Game Sprites\\back_imgs_to_be_filtered"
 animated_pngs_pre_gif_conversion = "C:\\Users\\ethan\\OneDrive\\Desktop\\Code\\Pokeball-Pokemon-Comparison\\Images\\Pokemon\\Game Sprites\\animated_pngs_for_gifs\\pngs\\Converted to gif"
 test_path = "C:\\Users\\ethan\\OneDrive\\Desktop\\Code\\Pokeball-Pokemon-Comparison\\Images\\Pokemon\\Test"
+staging_path = "C:\\Users\\ethan\\OneDrive\\Desktop\\Code\\Pokeball-Pokemon-Comparison\\Images\\Pokemon\\Game Sprites\\staging"
 file_ext = ""
+
 
 def add_leading_zero(path):
     files = os.listdir(path)
@@ -17,6 +17,7 @@ def add_leading_zero(path):
             new_full_path = os.path.join(path, "0"+f)
             print(new_full_path)
             os.rename(full_path, new_full_path)
+
 
 def replace_in_filename(path, replace, replace_with, just_print=False):
     files = os.listdir(path)
@@ -30,14 +31,15 @@ def replace_in_filename(path, replace, replace_with, just_print=False):
                 if not just_print:
                     os.rename(full_path, new_full_path)
 
+
 def replace_filename_in_all_dirs(replace, replace_with, just_print=False):
     replace_in_filename(game_sprite_path, replace, replace_with, just_print)
     replace_in_filename(back_sprites_to_filter_path, replace, replace_with, just_print)
     replace_in_filename(animated_pngs_pre_gif_conversion, replace, replace_with, just_print)
+
 
 def print_files_with(path, s):
     files = os.listdir(path)
     for f in files:
         if s in f:
             print(f)
-
