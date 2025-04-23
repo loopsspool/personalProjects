b71e944c8221777ab30382cc3a5435c9943c4d99
loopsspool <45133123+loopsspool@users.noreply.github.com>
2025-04-23 15:22:08 -0500

    Moved static data to reference data for easier maintenence

--- Pokeball-Pokemon-Comparison/Scripts/bulba_scraping_utils.py
+++ Pokeball-Pokemon-Comparison/Scripts/bulba_scraping_utils.py
@@ -90,29 +90,7 @@ def scrape(force=False):
 
 scrape()
 
-############################# BULBA TRANSLATORS #############################
-GAME_MAP = {
-    # Gen included so Gold doesn't trigger Golduck, Yellow trigger Yellow Core Minior, etc
-    "Gen1 Red_Blue": "1b",
-    "Gen1 Red_Green": "1g",
-    "Gen1 Yellow": "1y",
-    "Gen2 Crystal": "2c",
-    "Gen2 Gold": "2g",
-    "Gen2 Silver": "2s",
-    "Gen3 Emerald": "3e",
-    "Gen3 FRLG": "3f",
-    "Gen3 Ruby_Sapphire": "3r",
-    "Gen4 Diamond_Pearl": "4d",
-    "Gen4 HGSS": "4h",
-    "Gen4 Platinum": "4p",
-    "Gen5 BW_B2W2": "5b",   # TODO: 5b2 may exist? Check on any missing after
-    "Gen6 XY_ORAS": "6x",   # TODO: 6o may exist? Check on any missing after
-    "Gen7 SM_USUM": "7s",
-    "Gen7 LGPE": "7p",
-    "Gen8 SwSh": "8s",
-    # Doesn't look like bulba has any BDSP sprites... Can confirm by checking pokes unavailable in SwSh & LA
-    "Gen8 LA": "8a"
-}
+
 
 # TODO: Uncomment download function
 def ani_check_and_download(img, filename):
