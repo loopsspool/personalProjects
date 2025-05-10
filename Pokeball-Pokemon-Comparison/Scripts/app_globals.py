0ffd2f5853e179770430ef4424e1f19f0c242c60
loopsspool <45133123+loopsspool@users.noreply.github.com>
2025-05-10 13:42:04 -0500

    Add: Made paths relative, transfered start/stop params to main

--- Pokeball-Pokemon-Comparison/Scripts/app_globals.py
+++ Pokeball-Pokemon-Comparison/Scripts/app_globals.py
@@ -1,29 +1,9 @@
-import string # To access letters easily without having to type them myself in an array
-from db_utils import get_last_poke_num, populate_db
+import os
 
-# TODO: Have all functions utilize the start and to go after start denoters
-# TODO: This could be put in a text file, So I don't even have to write this after each crash. Each new pokemon would rewrite the line of the file where it picked up
-# This is so when I get kicked from the server I only have to write once where to pick up
-POKE_NUM_START_FROM = 1
-POKE_TO_GO_AFTER_START = 10000
-LAST_POKE_NUM = -1
-# TODO: Gotta figure out a better way to do this
-# try: LAST_POKE_NUM = get_last_poke_num()
-# except:
-#     populate_db()
-#     LAST_POKE_NUM = get_last_poke_num()
+PARENT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-if POKE_NUM_START_FROM < 1: POKE_NUM_START_FROM=1
-if POKE_NUM_START_FROM > LAST_POKE_NUM: POKE_NUM_START_FROM = LAST_POKE_NUM
-if POKE_TO_GO_AFTER_START < 1: POKE_TO_GO_AFTER_START=0
-if POKE_NUM_START_FROM + POKE_TO_GO_AFTER_START > LAST_POKE_NUM: POKE_TO_GO_AFTER_START = LAST_POKE_NUM - POKE_NUM_START_FROM
-POKE_NUM_END_AT = POKE_NUM_START_FROM + POKE_TO_GO_AFTER_START
-
-
-save_path_starter = "C:\\Users\\ethan\\OneDrive\\Desktop\\Code\\Pokeball-Pokemon-Comparison\\Images\\Pokemon"
-game_save_path = save_path_starter + "\\Game Sprites\\"
-gen6_menu_sprite_save_path = save_path_starter + "\\Menu Sprites\\Gen6\\"
-gen8_menu_sprite_save_path = save_path_starter + "\\Menu Sprites\\Gen8\\"
-home_menu_sprite_path = save_path_starter + "\\Menu Sprites\\HOME\\"
-drawn_save_path = save_path_starter + "\\Drawn\\"
-home_save_path = save_path_starter + "\\Home Sprites\\"
+GAME_SPRITE_PATH = os.path.join(PARENT_DIR, "Images\\Pokemon\\Game Sprites\\")
+DRAWN_SAVE_PATH = os.path.join(PARENT_DIR, "Images\\Pokemon\\Drawn\\")
+HOME_SAVE_PATH = os.path.join(PARENT_DIR, "Images\\Pokemon\\HOME Sprites\\")
+HOME_MENU_SAVE_PATH = os.path.join(PARENT_DIR, "Images\\Pokemon\\Menu Sprites\\HOME\\")
+POKEBALL_SAVE_PATH = os.path.join(PARENT_DIR, "Images\\Pokeballs\\")
