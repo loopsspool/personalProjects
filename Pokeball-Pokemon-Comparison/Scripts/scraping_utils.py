55bbdf0486b99d625fb32d9d420f4909bdc1df55
loopsspool <45133123+loopsspool@users.noreply.github.com>
2025-05-17 09:12:09 -0500

    Add: Added animation check from bulba_scraping and added organizational headers

--- Pokeball-Pokemon-Comparison/Scripts/scraping_utils.py
+++ Pokeball-Pokemon-Comparison/Scripts/scraping_utils.py
@@ -2,12 +2,18 @@ import urllib   # For downloading those images to my computer
 import os
 
 from db_utils import get_missing_poke_imgs_by_table, get_missing_pokeball_imgs
+from image_utils import save_first_frame, is_animated
 
-# NOTE: ALL DOWNLOADS MUST BE DONE IN THE FASHION BELOW
-    # Otherwise bulba has a check on if the site is being web scraped and it will block the download
-# This is to mask the fact I'm webscraping
-    # To use, call
-    # filename, headers = opener.retrieve(get_largest_png(img), path + save_name)
+
+
+
+#|================================================================================================|
+#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~[     DOWNLOADING UTILITIES     ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+#|================================================================================================|
+
+# TODO: Add scraping countermeasures: 1-3 sec wait between downloads, rotate IPs w proxy pools, session spoofing?
+
+# NOTE: ALL DOWNLOADS MUST BE DONE IN THE FASHION BELOW -- Otherwise bulba has a check on if the site is being web scraped and it will block the download
 opener = urllib.request.URLopener()
 opener.addheader('User-Agent', 'Mozilla/5.0')
 
@@ -15,16 +21,25 @@ def download_img(url, save_path):
     filename, headers = opener.retrieve(url, save_path)
 
 
-# This logic is constant coming from a script (bulba/wikidex/other) and does not matter which image type is being passed, 
-# So I can pull these parameters out of the scrape_imgs calls
-def generate_config_dict(starter_url, download_func, allow_download):
-    config_dict = {
-        "Site URL": starter_url,
-        "Site DL Logic Function": download_func,
-        "Allow Download": allow_download
-    }
-    return config_dict
+def determine_animation_status_before_downloading(img_url, save_path):
+    img_is_animated = is_animated(img_url)
+    if "-Animated" in save_path:
+        if img_is_animated:
+            download_img(img_url, save_path)
+        else:
+            print(f"Could not download animated img for {save_path.split("\\")[-1]}")
+    else: # Looking for still
+        if not img_is_animated:
+            download_img(img_url, save_path)
+        else: # Looking for still, but image is animated
+            save_first_frame(img_url, save_path)
 
+
+
+
+#|================================================================================================|
+#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~[     SCRAPING UTILITIES     ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+#|================================================================================================|
 
 def scrape_imgs(poke_num, filename_table, translate_func, exclusions, has_animation, save_path, config_dict):
     missing_imgs_dict = get_missing_poke_imgs_by_table(poke_num, filename_table) if filename_table != "pokeball_filenames" else get_missing_pokeball_imgs()
@@ -59,3 +74,14 @@ def translate_all_my_filenames_to_url(filename_dict, translate_func, exclude, st
 
 def convert_translated_filename_to_url(starter_url, translated_filename):
     return (starter_url + translated_filename.replace(" ", "_"))
+
+
+# This logic is constant coming from a script (bulba/wikidex/other) and does not matter which image type is being passed, 
+# So I can pull these parameters out of the scrape_imgs calls
+def generate_config_dict(starter_url, download_func, allow_download):
+    config_dict = {
+        "Site URL": starter_url,
+        "Site DL Logic Function": download_func,
+        "Allow Download": allow_download
+    }
+    return config_dict
