1bc34965b6ddf22aa4b4441f67e41c8cf2dcfe5c
loopsspool <45133123+loopsspool@users.noreply.github.com>
2025-05-21 19:42:29 -0500

    Add: Added mid-gen poke introduction list

--- Pokeball-Pokemon-Comparison/Scripts/translation_utils.py
+++ Pokeball-Pokemon-Comparison/Scripts/translation_utils.py
@@ -1,3 +1,9 @@
+import re
+
+#|================================================================================================|
+#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~[     TRANSLATIONS     ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+#|================================================================================================|
+
 def get_translated_game(my_filename, map_dict):
     for game, translation in map_dict.items():
         if "-Back" in my_filename:
@@ -10,7 +16,90 @@ def get_translated_universal_form(my_filename, mapping):
     for u_form, translation in mapping.items():
         if u_form in my_filename: return(translation)
     return ("")
-        
+
+
+def extract_gen_num_from_my_filename(my_filename):
+    gen_str = re.search(r' Gen(\d+)', my_filename) # Extracting gen from my filename
+    gen_int = int(gen_str.group(1))  # Getting just the gen number
+    return gen_int
+
+
+
+
+
+# This dict maps the few pokemon introduced later in the same gen for games that share sprites, so they are potentially denoted differently on websites
+ALT_GAME_MAP = {
+    # NOTE: Technically the gen5 starters have different animations (NOT statics) for b2w2 vs bw, but I'm not going to put that logic in since I'm axing alts
+    "BW_B2W2": {
+        ("Keldeo", "-Form_Resolute"),
+        ("Kyurem", "-Form_Black"),
+        ("Kyurem", "-Form_White"),
+        ("Landorus", "-Form_Therian"),
+        ("Thundurus", "-Form_Therian"),
+        ("Tornadus", "-Form_Therian")
+    },
+
+    # TODO: Exclude these from GAME_FALLBACK
+    # NOTE: SM_USUM is a GAME_FALLBACK for XY_ORAS, and vice versa
+    "XY_ORAS": {
+        ("Groudon", "-Form_Primal"),
+        ("Kyogre", "-Form_Primal"),
+        ("Hoopa", "-Form_Unbound"),
+        ("Altaria", "-Mega"),
+        ("Audino", "-Mega"),
+        ("Beedrill", "-Mega"),
+        ("Camerupt", "-Mega"), 
+        ("Diancie", "-Mega"),
+        ("Gallade", "-Mega"),
+        ("Glalie", "-Mega"),
+        ("Lopunny", "-Mega"), 
+        ("Metagross", "-Mega"),
+        ("Pidgeot", "-Mega"),
+        ("Rayquaza", "-Mega"),
+        ("Sableye", "-Mega"), 
+        ("Salamence", "-Mega"),
+        ("Sceptile", "-Mega"),
+        ("Sharpedo", "-Mega"),
+        ("Slowbro", "-Mega"), 
+        ("Steelix", "-Mega"),
+        ("Swampert", "-Mega"),
+        ("Pikachu", "-Form_Cosplay"),
+        ("Pikachu", "-Form_Cosplay_Belle"),
+        ("Pikachu", "-Form_Cosplay_Libre"), 
+        ("Pikachu", "-Form_Cosplay_PhD"),
+        ("Pikachu", "-Form_Cosplay_Pop_Star"),
+        ("Pikachu", "-Form_Cosplay_Rock_Star")
+    },
+
+    "SM_USUM": {
+        ("Blacephalon", ""),
+        ("Lycanroc", "-Form_Dusk"),
+        ("Naganadel", ""),
+        ("Necrozma", "-Form_Dusk_Mane"),
+        ("Necrozma", "-Form_Dawn_Wings"),
+        ("Necrozma", "-Form_Ultra"),
+        ("Pikachu", "-Form_Cap_Partner"),
+        ("Poipole", ""),
+        ("Stakataka", ""),
+        ("Zeraora", "")
+    }
+}
+
+
+def get_game_needing_special_translation_for_mid_gen_pokes(my_filename):
+    for game, pokes in ALT_GAME_MAP.items():
+        for poke in pokes:
+            if game in my_filename and poke[0] in my_filename and poke[1] in my_filename:
+                return game
+            
+    return None
+
+
+
+
+#|================================================================================================|
+#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~[     DEFINITIONS     ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+#|================================================================================================|
 
 # This is to filter out these forms when translating species forms
 # The exceptions that have a universal AND species form (See 555 Galarian Darmanitan Zen form) have their own unique form id generated in db_utils > FORM_EXCEPTION_POKEMON
