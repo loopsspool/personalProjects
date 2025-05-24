6e6240517e578f5738446c7af0251d8fa82c9584
loopsspool <45133123+loopsspool@users.noreply.github.com>
2025-05-24 14:19:59 -0500

    Add: Proper processing of webm for animation check and downloading the first frame of webm

--- Pokeball-Pokemon-Comparison/Scripts/image_utils.py
+++ Pokeball-Pokemon-Comparison/Scripts/image_utils.py
@@ -2,23 +2,69 @@ from PIL import Image   # For converting URL image data to PIL Image object
 import requests # To retrieve webpages
 from bs4 import BeautifulSoup   # To parse webpages
 import re
+import os
+# import cv2  # To save first frame of webm
+# import numpy as np  # To parse webm
+import subprocess
+import tempfile     # To read webm
 
 
-def is_animated(link):
-    # NOTE: Works on animated pngs
-    # Converting URL image to PIL Image Object
-    img = Image.open(requests.get(link, stream = True).raw)
-    # Checking if it is an animated image
-    return(img.is_animated)
+def is_animated(url):
+    file_type = f".{url.split(".")[-1]}"
+
+    if file_type in (".png", ".gif"):
+        img = Image.open(requests.get(url, stream = True).raw)
+        return(img.is_animated)
+    elif file_type in (".webm"):
+        return(True)
+    else:
+        raise RuntimeError("Unkown file type")
 
 
-def save_first_frame(link, save_path):
-    img = Image.open(requests.get(link, stream = True).raw)
+# TODO: Properly convert gif to png to preserve color
+def save_first_frame_of_png_or_gif(url, save_path):
+    img = Image.open(requests.get(url, stream = True).raw)
     first_frame = img.copy()    # Returns just first frame
     first_frame.save(save_path)
 
 
-# TODO: Incorporate the below to save each frame of an animated img
+# TODO: No webm videos have transparency, will need to set a different save file for these to remove later
+    # Or write function to remove them now
+def save_first_frame_of_webm(url, save_path):
+    # Download the video
+    response = requests.get(url)
+    response.raise_for_status()
+
+    # TODO: This always asks to overwrite an existing file, fix
+    # Write to a temp video file
+    with tempfile.NamedTemporaryFile(suffix=".webm", delete=False) as tmp:
+        tmp.write(response.content)
+        tmp.flush()
+        temp_video_path = tmp.name
+
+    # Write to a temp image file
+    with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as image_file:
+        temp_image_path = image_file.name
+
+    # Extract first frame using ffmpeg
+    subprocess.run([
+        "ffmpeg",
+        "-i", temp_video_path,
+        "-vf", "select=eq(n\\,0)",
+        "-vframes", "1",
+        "-pix_fmt", "rgba",  # keep transparency
+        temp_image_path
+    ], check=True)
+
+    # Save Img
+    img = Image.open(temp_image_path)
+    img.save(save_path)
+
+    os.remove(temp_video_path)
+    os.remove(temp_image_path)
+
+
+# TODO: Incorporate the below to save each frame of an animated png
 # img = Image.open(os.path.join(POKEBALL_SAVE_PATH, file))
 #         frame=0
 #         try:
@@ -33,9 +79,9 @@ def save_first_frame(link, save_path):
 
 def bulba_get_largest_png(img_page_soup):
     # Find the biggest image location
-    biggest_link = img_page_soup.find("div", "fullImageLink")
-    # Return its link
-    return (biggest_link.a.get("href"))
+    biggest_url = img_page_soup.find("div", "fullImageLink")
+    # Return its url
+    return (biggest_url.a.get("href"))
 
 
 def wikidex_get_largest_img(img_page_soup):
