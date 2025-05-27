f530c8cabbe71eb37b44623f391a35c33f485779
loopsspool <45133123+loopsspool@users.noreply.github.com>
2025-05-26 19:45:01 -0500

    Add: Added all file paths in use, made save paths a dict for reuseability

--- Pokeball-Pokemon-Comparison/Scripts/app_globals.py
+++ Pokeball-Pokemon-Comparison/Scripts/app_globals.py
@@ -24,17 +24,21 @@ PARENT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 DB_NAME = "pokedex.db"
 DB_PATH = os.path.join(PARENT_DIR, DB_NAME)
 
-# Actual save paths
-GAME_SPRITE_SAVE_PATH = os.path.join(PARENT_DIR, "Images\\Pokemon\\Game Sprites\\")
-DRAWN_SAVE_PATH = os.path.join(PARENT_DIR, "Images\\Pokemon\\Drawn\\")
-HOME_SAVE_PATH = os.path.join(PARENT_DIR, "Images\\Pokemon\\HOME Sprites\\")
-HOME_MENU_SAVE_PATH = os.path.join(PARENT_DIR, "Images\\Pokemon\\Menu Sprites\\HOME\\")
-POKEBALL_SAVE_PATH = os.path.join(PARENT_DIR, "Images\\Pokeballs\\")
-# Staging/testing paths
-GIF_SAVE_PATH = os.path.join(PARENT_DIR, "Images\\Pokemon\\Game Sprites\\gif\\")
-WEBM_SAVE_PATH = os.path.join(PARENT_DIR, "Images\\Pokemon\\Game Sprites\\webm\\")
-TEST_PATH = os.path.join(PARENT_DIR, "Images\\Pokemon\\Test\\")
-STAGING_PATH = os.path.join(PARENT_DIR, "Images\\Pokemon\\Game Sprites\\staging\\")
+
+SAVE_PATHS = {
+    # Actual save paths
+    "GAME_SPRITE" : os.path.join(PARENT_DIR, "Images\\Pokemon\\Game Sprites\\"),
+    "HOME" : os.path.join(PARENT_DIR, "Images\\Pokemon\\HOME Sprites\\"),
+    "HOME_MENU" : os.path.join(PARENT_DIR, "Images\\Pokemon\\Menu Sprites\\HOME\\"),
+    "DRAWN" : os.path.join(PARENT_DIR, "Images\\Pokemon\\Drawn\\"),
+    "POKEBALL" : os.path.join(PARENT_DIR, "Images\\Pokeballs\\"),
+    # Staging/testing paths
+    "GIF" : os.path.join(PARENT_DIR, "Images\\Pokemon\\Game Sprites\\gif\\"),
+    "WEBM" : os.path.join(PARENT_DIR, "Images\\Pokemon\\Game Sprites\\webm\\"),
+    "NEED_TRANSPARENCY" : os.path.join(PARENT_DIR, "Images\\Pokemon\\Game Sprites\\need_transparency\\"),    # Used for saving stills from webm
+    "TEST" : os.path.join(PARENT_DIR, "Images\\Pokemon\\Test\\"),
+    "STAGING" : os.path.join(PARENT_DIR, "Images\\Pokemon\\Game Sprites\\staging\\")
+}
 
 
 
@@ -43,8 +47,17 @@ STAGING_PATH = os.path.join(PARENT_DIR, "Images\\Pokemon\\Game Sprites\\staging\
 #|~~~~~~~~~~~~~~~~~~~~~~~~~~~[     SETS OF SAVED IMG FILENAMES     ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
 #|================================================================================================|
 
-ALL_SAVED_GAME_SPRITES = set(os.listdir(GAME_SPRITE_SAVE_PATH))
-ALL_SAVED_HOME_SPRITES = set(os.listdir(HOME_SAVE_PATH))
-ALL_SAVED_HOME_MENU_IMAGES = set(os.listdir(HOME_MENU_SAVE_PATH))
-ALL_SAVED_DRAWN_IMAGES = set(os.listdir(DRAWN_SAVE_PATH))
-ALL_SAVED_POKEBALL_IMAGES = set(os.listdir(POKEBALL_SAVE_PATH))
+
+# Staging/testing paths
+ALL_SAVED_GIFS = set(os.listdir(SAVE_PATHS["GIF"]))
+ALL_SAVED_WEBMS = set(os.listdir(SAVE_PATHS["WEBM"]))
+ALL_SAVED_NEED_TRANSPARENCY_IMGS = set(os.listdir(SAVE_PATHS["NEED_TRANSPARENCY"]))
+ALL_SAVED_TEST_IMGS = set(os.listdir(SAVE_PATHS["TEST_PATH"]))
+ALL_SAVED_STAGING_IMGS = set(os.listdir(SAVE_PATHS["STAGING_PATH"]))
+# Actual save paths
+ALL_SAVED_GAME_SPRITES = set(os.listdir(SAVE_PATHS["GAME_SPRITE"]))
+ALL_SAVED_GAME_SPRITES = ALL_SAVED_GAME_SPRITES | ALL_SAVED_NEED_TRANSPARENCY_IMGS   # This unions the two so db recognizes file existence and I don't download need transparency images every time I start a new scrape session. # TODO: Can remove once transparency issue resolved
+ALL_SAVED_HOME_SPRITES = set(os.listdir(SAVE_PATHS["HOME"]))
+ALL_SAVED_HOME_MENU_IMAGES = set(os.listdir(SAVE_PATHS["HOME_MENU"]))
+ALL_SAVED_DRAWN_IMAGES = set(os.listdir(SAVE_PATHS["DRAWN"]))
+ALL_SAVED_POKEBALL_IMAGES = set(os.listdir(SAVE_PATHS["POKEBALL"]))
