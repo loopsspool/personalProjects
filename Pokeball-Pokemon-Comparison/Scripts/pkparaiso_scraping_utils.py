61c71939dcc60e2cf3b75259a85f525e7b2a720c
loopsspool <45133123+loopsspool@users.noreply.github.com>
2025-06-03 11:31:51 -0500

    Add: Comments for clarity

--- Pokeball-Pokemon-Comparison/Scripts/pkparaiso_scraping_utils.py
+++ Pokeball-Pokemon-Comparison/Scripts/pkparaiso_scraping_utils.py
@@ -1,16 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
 from app_globals import save_directories
 from scraping_utils import *
 
-# NOTE: For all other relevant sources, pkparaiso has a downloadable zip... Except these
+# NOTE: For all other relevant sources, pkparaiso has a downloadable zip... Except these (Gen8 SwSh Animated Back Sprites (Both Shiny and regular))
 SCRAPING_PAGE = "https://www.pkparaiso.com/espada_escudo/sprites_pokemon_espalda.php"
 PKPARAISO_STARTER_URL = "https://www.pkparaiso.com"
 
 
+
+
+# TODO: .......... It appears I have most of these already? Scrape page anyways and translate to see if I'm missing any (do a db search via game id, sprite id, poke id, etc)
 def pkparaiso_scrape_swsh_ani_back_sprites(allow_download=False):
-    wikidex_scrape_config = generate_config_dict(PKPARAISO_STARTER_URL, pkparaiso_get_img, allow_download)
+    # NOTE: Passing in empty string as starter url so pkparaiso_translate can return full url as "filename translation"
+    pkparaiso_scrape_config = generate_config_dict("", pkparaiso_get_img, allow_download)
 
     # -1 for poke_num since this is only scaping one page of predetermined game, sprite types, and pokes. Meant to be run once in full.
-    scrape_imgs(-1, "obtainable_game_filenames", pkparaiso_translate, exclusions=None, has_animation=True, save_path=save_directories["Game Sprites"]["path"], config_dict=wikidex_scrape_config)
+    scrape_imgs(-1, "obtainable_game_filenames", pkparaiso_translate, exclusions=None, has_animation=True, save_path=save_directories["Game Sprites"]["path"], config_dict=pkparaiso_scrape_config)
 
 
 
@@ -31,6 +38,7 @@ def pkparaiso_get_img(url, save_path, allow_download, has_animation=False):
             # TODO: Will just be grabbing link from item on page
             #img_url = bulba_get_largest_png(img_page_soup)
 
+            # TODO: Put this into a func in scraping_utils and change it in bulba & wikidex scraping_utils
             if has_animation:
                 determine_animation_status_before_downloading(img_url, save_path)
             else:
@@ -43,7 +51,14 @@ def pkparaiso_get_img(url, save_path, allow_download, has_animation=False):
 #|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~[     TRANSLATIONS     ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
 #|================================================================================================|
 
-# As opposed to other web scrapers, instead of crafting the site filename after my filename, I will be discovering my filename from the site filename
-# This is due to a relatively small pool of pokemon (thus forms), knowing the game and sprite type, and having to cycle through a single page instead of generating urls
-def pkparaiso_translate(pkp_filename):
+# TODO: Implement the below
+# First looks through pkparaiso translate dict for passed poke_name
+# If not in dict, return None -- causing file to be skipped for download
+# Then check my_filename is a Gen8 SwSh Back (both shiny/animated optional since contains shinies and stills can be from first frame of gif), if not return None
+# Have a global dict mapping poke_name : url
+    # Make func for this -- scrapes page, gets caption and url to gif
+    # Call at top of file (so accessable by this) if __file__ == main or pkparaiso_scraping_utils (lookup how to do)
+# If poke on page (in translate dict) and my_filename is gen8 SwSh back, return url for poke_name (fine to do since made starter url empty string)
+
+def pkparaiso_translate(my_filename, poke_info):
     pass
