1b6152366054092c83c1ee785e8be636be9614bf
loopsspool <45133123+loopsspool@users.noreply.github.com>
2025-06-13 13:24:50 -0500

    Refactor: Tweaked whitespace in filenaming and edge case translations. Created deterministic translation functions. Also added bank scraping

--- Pokeball-Pokemon-Comparison/Scripts/pokewiki_scraping_utils.py
+++ Pokeball-Pokemon-Comparison/Scripts/pokewiki_scraping_utils.py
@@ -2,8 +2,10 @@ from pokewiki_translation_mapping import *
 from db_utils import get_poke_name, get_form_name, get_pokeball_name, get_pokeball_img_type_name, get_all_form_names_for_poke
 from app_globals import *
 from scraping_utils import *
-#from image_utils import pokewiki_get_largest_img
+from image_utils import pokewiki_get_largest_img
 from translation_utils import *
+
+
 
 
 # WEB DATA
@@ -15,7 +17,8 @@ def pokewiki_scrape_pokemon(poke_num, allow_download=False):
     pokewiki_scrape_config = generate_config_dict(POKEWIKI_STARTER_URL, pokewiki_get_img, allow_download)
 
     scrape_imgs(poke_num, "obtainable_game_filenames", pokewiki_translate, exclusions=pokewiki_doesnt_have_images_for, has_animation=True, save_path=save_directories["Game Sprites"]["path"], config_dict=pokewiki_scrape_config)
-    scrape_imgs(poke_num, "obtainable_home_filenames", pokewiki_translate, exclusions=None, has_animation=True, save_path=save_directories["HOME"]["path"], config_dict=pokewiki_scrape_config)
+    scrape_imgs(poke_num, "obtainable_home_filenames", pokewiki_translate, exclusions=pokewiki_doesnt_have_images_for, has_animation=True, save_path=save_directories["HOME"]["path"], config_dict=pokewiki_scrape_config)
+    scrape_imgs(poke_num, "bank_filenames", pokewiki_translate, exclusions=None, has_animation=False, save_path=save_directories["BANK"]["path"], config_dict=pokewiki_scrape_config)
 
 
 
@@ -29,16 +32,9 @@ def pokewiki_get_img(url, save_path, allow_download, has_animation=False):
     img_page_soup = img_exists_at_url(url)
     
     if allow_download:
-        # TODO: Implement pokewiki get largest
-        # file w only 1 size: https://www.pokewiki.de/Datei:Pok%C3%A9monsprite_1016_Schillernd_HOME.png
-        # file w different sizes: https://www.pokewiki.de/Datei:Pok%C3%A9monsprite_1016_Schillernd_KAPU.gif
         img_url = pokewiki_get_largest_img(img_page_soup)
-
-        # TODO: Put this into a func in scraping_utils and change it in bulba & wikidex scraping_utils
-        if has_animation:
-            determine_animation_status_before_downloading(img_url, save_path)
-        else:
-            download_img(img_url, save_path)
+        # TODO: Test this, if it works can remove conditionals for bulba & wikidex too
+        determine_animation_status_before_downloading(img_url, save_path)
     
 
 def pokewiki_doesnt_have_images_for(my_filename):
@@ -113,21 +109,29 @@ def get_pokewiki_universal_form(poke_num, form_name):
 def pokewiki_translate(my_filename, poke_info):
     # poke_info == (poke_num, form_id)
     poke_num_int = poke_info[0]
-    poke_num_leading_zeros = f" {str(poke_num_int).zfill(3)}"   # Pokewiki only uses leading zeros up to 3
+    poke_num_leading_zeros = str(poke_num_int).zfill(3)   # Pokewiki only uses leading zeros up to 3
     form_name = get_form_name(poke_info[1])
     
-    # TODO: Will need to rethink universal forms for pokewiki... Default always "", next form (say galarian) "a", next form (say alolan) "b", "c", etc. -- Dynamic denoters not static
     translated_form = get_pokewiki_translated_form(poke_info, my_filename)
-    form_tag = f" {translated_form}" if translated_form != "" else ""
+    form_tag = translated_form if translated_form != "" else ""
     gigantamax_tag = determine_gigantamax_tag(poke_num_int, form_name)
-    female_tag = " Weiblich" if "-f" in form_name else ""
+    female_tag = determine_female_tag(poke_num_int, form_name)
     back_tag = " Rückseite" if "-Back" in my_filename else ""
     shiny_tag = " Schillernd" if "-Shiny" in my_filename else ""
-    platform = get_translated_game(my_filename, POKEWIKI_GAME_MAP) if "HOME" not in my_filename else " HOME"
+    platform = determine_platform_tag(my_filename)
     file_ext = ".png" if "-Animated" not in my_filename else ".gif"
 
     pokewiki_filename = f"Pokémonsprite {poke_num_leading_zeros}{form_tag}{gigantamax_tag}{female_tag}{back_tag}{shiny_tag}{platform}{file_ext}"
     return (pokewiki_filename)
+
+
+def determine_female_tag(poke_num, form_name):
+    # For some reason in pokewiki, if poke was introduced gen4 or before, its noted as female. Gen5+ its noted as <poke_num>a (like another form)...
+    if "-f" in form_name:
+        if poke_num < 494: return (" Weiblich")
+        else: return ("a")
+    else: 
+        return ("")
 
 
 def determine_gigantamax_tag(poke_num, form_name):
@@ -139,3 +143,9 @@ def determine_gigantamax_tag(poke_num, form_name):
             gmax_denoter = "g1"
 
     return gmax_denoter
+
+
+def determine_platform_tag(my_filename):
+    if " HOME" in my_filename: return (" HOME")
+    elif " BANK" in my_filename: return (" BANK")
+    else: return(get_translated_game(my_filename, POKEWIKI_GAME_MAP, POKEWIKI_ALT_GAME_MAP))
